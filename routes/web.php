e4a1273cca2434e9bcf305ed5bf74b1b1cd0e3f1
Luthiano Miguel <luthianomiguel@gmail.com>
2025-10-02 16:00:15 -0300

    criado a rota products.destroy

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ Route::post('/products',[ProductController::class, "store"])->name("products.sto
 
 Route::get('/products{product}/edit',[ProductController::class, "edit"])->name("products.edit");
 Route::put('/products/{product}',[ProductController::class, "update"])->name("products.update");
+
+Route::delete('/products/{product}',[ProductController::class, "destroy"])->name("products.destroy");

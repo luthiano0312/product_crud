ff5cdda0e9c6da7acab7d923b506fff3dc93aaa2
Luthiano Miguel <luthianomiguel@gmail.com>
2025-10-02 15:22:45 -0300

    criado a view create de product

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastro</title>
+</head>
+<body>
+    <h1>Cadastro de produtos</h1>
+
+    <form action="{{ route('products.store') }}" method="post">
+        @csrf
+
+        <label for="name">Nome: </label>
+        <input type="text" name="name">
+
+        <label for="amount">Quantidade: </label>
+        <input type="text" name="amount">
+
+        <label for="price">Preço: </label>
+        <input type="text" name="price">
+
+        <input type="submit" valeu="cadastrar">
+    </form>
+</body>
+</html>

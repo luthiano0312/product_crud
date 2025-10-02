486f7154f38c3da88ffcd5142fa5787443f8cf72
Luthiano Miguel <luthianomiguel@gmail.com>
2025-10-02 15:55:03 -0300

    criado as rotas products.edit e products.update

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ Route::get('/products',[ProductController::class, "index"])->name("products.inde
 
 Route::get('/products/create',[ProductController::class, "create"])->name("products.create");
 Route::post('/products',[ProductController::class, "store"])->name("products.store");
+
+Route::get('/products{product}/edit',[ProductController::class, "edit"])->name("products.edit");
+Route::put('/products/{product}',[ProductController::class, "update"])->name("products.update");
